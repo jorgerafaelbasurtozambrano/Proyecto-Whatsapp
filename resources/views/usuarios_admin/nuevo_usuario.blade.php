5225e2494a095a358ab513dee2cf8d3a181ec230
unknown <jorgezambrano_64@hotmail.com>
2019-09-04 15:33:24 -0500

    hgfhgf

--- resources/views/usuarios_admin/nuevo_usuario.blade.php
+++ resources/views/usuarios_admin/nuevo_usuario.blade.php
@@ -1,22 +1,24 @@
 <div class="row">
   <div class="col-md-12 col-sm-12 col-xs-12">
   <div class="x_title">
-            <h2>Nuevo Usuario</h2>
+            <h2><i class="fa fa-whatsapp"></i> Nuevo Usuario</h2>
             <div class="clearfix"></div>
   </div>
     <div class="x_panel">
         <div class="x_content">
             <br />
-            <form data-parsley-validate class="form-horizontal form-label-left">
-            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                        <input type="text" class="form-control has-feedback-left" id="nombre_usuarionuevo" placeholder="Nombres">
-                        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                      </div>
+            <form data-parsley-validate class="form-horizontal form-label-left" style="margin:0 0 0 13%;">
+            <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+              <label for="nombre">Nombre de usuario:</label>      
+              <input type="text" class="form-control has-feedback-left btn-form-nuevouser" id="nombre_usuarionuevo" placeholder="Nombres" style="margin-bottom: 20px;" name="nombre">
+                    <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+              </div>
 
-                      <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                        <input type="number" class="form-control has-feedback-left" id="numero_usuarionuevo" placeholder="Numero de Telefono">
-                        <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
-                      </div>
+              <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback ">
+              <label for="nombre">Numero de usuario:</label>  
+                    <input type="number" class="form-control has-feedback-left btn-form-nuevouser" id="numero_usuarionuevo" placeholder="Numero de Telefono">
+                    <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
+               </div>
 
 
                       <div class="form-group text-center">
